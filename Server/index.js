8b5c68e06f1936978e654347b084f4b4a810e18f
Nima <lendey.nima@gmail.com>
2018-10-15 17:36:09 -0500

    adding grunt

--- Server/index.js
+++ Server/index.js
@@ -16,6 +16,12 @@
 
 
 
+
+
+
+
+
+
 // //FROM GRUNT PP SLIDE 15: CONFIGURE THE PORT
 // var express = require('express');
 
@@ -30,6 +36,14 @@
 // app.listen(app.get('port'), function(){
 // 	console.log('Express started on http://localhost:' + app.get('port'));
 // });
+
+
+
+
+
+
+
+
 
 
 
@@ -53,6 +67,13 @@
 // http.createServer(app).listen(3000, function(){
 // 	console.log('Express server listening on port ' + 3000);
 // })
+
+
+
+
+
+
+
 
 
 
@@ -85,45 +106,96 @@
 
 
 
-var http = require('http');
-var express = require('express');
 
-var app = express();
 
-app.use(function(req, res, next){
-    console.log('Request from ' + req.ip);
-    next();//Every middleware needs the next to complete the callback or else the request will never end
-  });
 
-  app.get('/',function(req,res){
-	res.send('Hello Nima & Middleware World!');
-});
 
-app.get('/about/directions', function(req, res){  //THE MORE SPECIFIC ROUTE NEEDS TO COME FIRST 
-	res.send('How to Find Us!');                //EXAMPLE: /ABOUT GOES AFTER /ABOUT/DIRECTIONS
-});
 
-  app.get('/about', function(req, res){  //THIS WILL ADD MORE ROUTES TO YOUR URL TO A DIFFERENT PAGE
-	res.send('About Us!');              //SENDS BACK PAGE FOR 'ABOUT US!'
-});
+
+
+
+
+// var http = require('http');
+// var express = require('express');
+// var path = require('path');
+
+// var app = express();
+
+// // THE APP.USE BELOW NEEDS TO GO BEFORE ANY ROUTES "APP.GET"
+// //THE "PATH.NORMALIZE" TAKES "(__DIRNAME)" AND WILL FIND THE PATH TO THAT FILE ON YOUR HARD DRIVE
+// app.use(express.static(path.normalize(__dirname) + '/public'));  //ANY STATIC RESOURCE WILL GO THROUGH PUBLIC FOLDER AUTOMATICALLY
+
+
+// app.use(function(req, res, next){
+//     console.log('Request from ' + req.ip);
+//     next();//Every middleware needs the next to complete the callback or else the request will never end
+//   });
+
+
+//   app.get('/',function(req,res){
+// 	res.send('Hello Nima & Middleware World!');
+// });
+
+// app.get('/about/directions', function(req, res){  //THE MORE SPECIFIC ROUTE NEEDS TO COME FIRST 
+// 	res.send('How to Find Us!');                //EXAMPLE: /ABOUT GOES AFTER /ABOUT/DIRECTIONS
+// });
+
+//   app.get('/about', function(req, res){  //THIS WILL ADD MORE ROUTES TO YOUR URL TO A DIFFERENT PAGE
+// 	res.send('About Us!');              //SENDS BACK PAGE FOR 'ABOUT US!'
+// });
+
+
+
+
+
+
+
+
+
+
 
 
 // WE USE THIS MIDDLEWARE CALLBACK IF THE REQUEST FROM ABOVE WAS NOT COMPLETED
 //SO WE PUT IT AFTER THE APP.GET() FUNCTION
-app.use(function(req, res){
-	res.type('text/plan');
-	res.status(404);
-	res.send('404 NIMA YOUR page Not Found (404 - problem with your request');
+//app.use(function(req, res){
+// 	res.type('text/plan');
+// 	res.status(404);
+// 	res.send('404 NIMA YOUR page Not Found (404 - problem with your request');
+// });
+
+// app.use(function(err, req, res, next){
+// 	console.error(err.stack);
+// 	res.type('text/plan');
+// 	res.status(500);
+// 	res.send('500 Sever Error');
+// });
+
+// http.createServer(app).listen(3000, function(){
+// 	console.log('Express server listening on port ' + 3000);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EXPRESS.JS PP SLIDE 32 (LAST SLIDE)
+var express = require('express');
+var config = require('./config/config');  // not a node module so we have to add the path
+
+var app = express();
+var port = process.env.port || 3000;
+
+require('./config/express')(app, config);
+require('http').createServer(app).listen(port, function () {
+    console.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
 });
-
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.type('text/plan');
-	res.status(500);
-	res.send('500 Sever Error');
-});
-
-http.createServer(app).listen(3000, function(){
-	console.log('Express server listening on port ' + 3000);
-})
-
