a01e86baf814907b367de59170ac6171db942058
Nima <lendey.nima@gmail.com>
2018-10-29 20:40:44 -0500

    changes

--- PracticeMidterm/app/models/todo.js
+++ PracticeMidterm/app/models/todo.js
@@ -1,11 +1,13 @@
 var Mongoose = require('mongoose');
 var Schema = Mongoose.Schema;
 
+
 var todoSchema = new Schema({
     Todo: { type: String, require: true },
     Priority: { type: String, require: true, enum:['Critical','High','Medium','Low']},
     DateDue: { type: Date, default: Date.now}
 });
+
 
 module.exports =
     Mongoose.model('todo', todoSchema);
