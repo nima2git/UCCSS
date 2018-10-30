937c278a3fe307e48657d73cba3c3f4a656ea94a
Nima <lendey.nima@gmail.com>
2018-10-29 20:36:46 -0500

    practice exam upload

---
+++ PracticeMidterm/app/models/todo.js
@@ -0,0 +1,11 @@
+var Mongoose = require('mongoose');
+var Schema = Mongoose.Schema;
+
+var todoSchema = new Schema({
+    Todo: { type: String, require: true },
+    Priority: { type: String, require: true, enum:['Critical','High','Medium','Low']},
+    DateDue: { type: Date, default: Date.now}
+});
+
+module.exports =
+    Mongoose.model('todo', todoSchema);
---
+++ PracticeMidterm/config/config.js
@@ -0,0 +1,14 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..');
+    // env = process.env.NODE_ENV || 'production';
+
+var config = {
+    // production: {
+        root: rootPath,
+        app: { name: 'Todo' },
+        port: 3300,
+        db: 'mongodb://127.0.0.1/todo' 
+    // }
+};
+
+module.exports = config;
