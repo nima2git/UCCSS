033f68ef35aae851761fe33217d29df7ae3d5d3f
Nima <lendey.nima@gmail.com>
2018-12-01 14:56:33 -0600

    12/1 changes at stone creek

--- Server/client/src/resources/elements/nav-bar.js
+++ Server/client/src/resources/elements/nav-bar.js
@@ -2,22 +2,30 @@ import { inject } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 
 @inject(Router)
-
 export class NavBar {
     constructor(router) {
-        this.authenticated = false;
         this.router = router;
+        this.authenticated = false;
         this.email = "";
         this.password = "";
     }
+    //The attached code block makes it so the links that are clicked are highlighted 'home', 'users',
+    //'helpTickets'.
+    attached() {
+        $('.navbar-nav a').on('click', function () {
+            $('.navbar-nav').find('li.active').removeClass('active');
+            $(this).parent('li').addClass('active');
+        });
+    }
+
     login() {
         console.log(this.email);
         console.log(this.password);
         this.authenticated = true;
         this.router.navigate('home');
     }
-    logout(){
+    logout() {
         this.authenticated = false;
         this.router.navigate('landing');
-        }
     }
+}
