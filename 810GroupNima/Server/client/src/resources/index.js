bdbba26e8c9daac535733ee43a014a80299bb77a
Nima <lendey.nima@gmail.com>
2018-12-15 14:18:49 -0600

    removed 810groupfile

--- 810GroupNima/Server/client/src/resources/index.js
+++
@@ -1,7 +0,0 @@
-export function configure(config) {
-  config.globalResources([
-    './elements/nav-bar',
-    './value-converters/format-date'
-  ]);
-
-}
--- 810GroupNima/Server/client/src/resources/value-converters/format-date.js
+++
@@ -1,6 +0,0 @@
-export class FormatDateValueConverter {
-    toView(value) {
-        let myDate = new Date(value);
-        return myDate.toLocaleDateString() + "<br/>" + myDate.toLocaleTimeString();
-    }
-}
