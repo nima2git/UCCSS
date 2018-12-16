e38d55b4cd0f6c636f3a9a71563a8d1c85ecd094
Nima <lendey.nima@gmail.com>
2018-12-16 14:30:07 -0600

    final changes...again lol 12/16

--- Server/client/src/resources/data/help-ticket-object.js
+++ Server/client/src/resources/data/help-ticket-object.js
@@ -11,9 +11,6 @@ export class HelpTicket {
 
     async getHelpTickets(userObj) {
         let url = this.HELP_TICKET_SERVICE;
-        if (userObj.role == 'user') {
-            url += '/user/' + userObj._id;
-        }
         let response = await this.data.get(url);
         if (!response.error) {
             this.helpTicketsArray = response;
